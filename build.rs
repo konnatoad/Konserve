9ac69f9a9383df8c700d3c27e7c2755b6beecda5
konnatoad <severi.moisare@gmail.com>
2025-03-23 21:44:05 +0200

    "feat: Add embed_resource for Windows RC file"

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 #[cfg(windows)]
 fn main() {
     println!("cargo:rustc-link-arg=/SUBSYSTEM:windows");
+
+    let _ = embed_resource::compile("assets/windows.rc", embed_resource::NONE);
 }
 
 #[cfg(not(windows))]
