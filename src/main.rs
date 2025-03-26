837b361bed04d0576c072a60ea668b745cc9e0df
konnatoad <severi.moisare@gmail.com>
2025-03-26 21:37:12 +0200

    "fix: Remove extra space from UI, cleanup file selection informational messages"

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use egui::IconData;
 use rfd::FileDialog;
 use serde::{ Deserialize, Serialize };
 
-// if !icon then fn fuck you
+// if !icon then fuck you
 pub fn load_icon_image() -> Arc<IconData> {
     let image_bytes = include_bytes!("../assets/icon.png");
     let image = image
@@ -88,7 +88,7 @@ impl eframe::App for GUIApp {
 
                 egui::ScrollArea
                     ::vertical()
-                    .max_height(290.0)
+                    .max_height(285.0)
                     .show(ui, |ui| {
                         ui.set_width(ui.available_width());
                         let mut to_remove = None;
@@ -107,13 +107,13 @@ impl eframe::App for GUIApp {
                                 }
 
                                 if path.exists() {
-                                    ui.label("✅");
+                                    ui.label("✅").on_hover_text("This path exists");
                                 } else {
-                                    ui.label("❌");
+                                    ui.label("❌").on_hover_text("This path does not exist");
                                 }
 
                                 if ui.button("Browse").clicked() {
-                                    if let Some(p) = FileDialog::new().pick_file() {
+                                    if let Some(p) = FileDialog::new().pick_folder() {
                                         *path = p;
                                     }
                                 }
@@ -161,6 +161,10 @@ impl eframe::App for GUIApp {
                     self.template_editor = false;
                 }
 
+                ui.separator();
+
+                ui.label("File names and extensions have to be manually typed in.");
+
                 return;
             }
 
@@ -182,154 +186,179 @@ impl eframe::App for GUIApp {
                 }
             });
 
-            ui.add_space(4.0);
+            if !self.selected_folders.is_empty() {
+                ui.add_space(4.0);
 
-            // selected paths
-            let mut to_remove = None;
-            egui::ScrollArea
-                ::vertical()
-                .max_height(200.0)
-                .show(ui, |ui| {
-                    ui.set_width(ui.available_width());
-                    for (i, path) in self.selected_folders.iter().enumerate() {
-                        if ui.button(path.display().to_string()).clicked() {
-                            to_remove = Some(i);
+                // selected paths
+                let mut to_remove = None;
+                egui::ScrollArea
+                    ::vertical()
+                    .max_height(240.0)
+                    .show(ui, |ui| {
+                        ui.set_width(ui.available_width());
+                        for (i, path) in self.selected_folders.iter().enumerate() {
+                            if ui.button(path.display().to_string()).clicked() {
+                                to_remove = Some(i);
+                            }
                         }
-                    }
-                });
+                    });
+                if let Some(i) = to_remove {
+                    self.selected_folders.remove(i);
+                }
 
-            if let Some(i) = to_remove {
-                self.selected_folders.remove(i);
-            }
+                ui.add_space(4.0);
 
-            ui.add_space(4.0);
-
-            if ui.button("Clear All").clicked() {
-                self.selected_folders.clear();
+                if ui.button("Clear All").clicked() {
+                    self.selected_folders.clear();
+                }
             }
 
             ui.separator();
 
-            if ui.button("Load Template").clicked() {
-                if let Some(path) = FileDialog::new().add_filter("JSON", &["json"]).pick_file() {
-                    if let Ok(data) = fs::read_to_string(&path) {
-                        if let Ok(template) = serde_json::from_str::<BackupTemplate>(&data) {
-                            let mut valid = Vec::new();
-                            let mut skipped = Vec::new();
+            ui.horizontal(|ui| {
+                ui.vertical(|ui| {
+                    //template
+                    if ui.button("Load Template").clicked() {
+                        if
+                            let Some(path) = FileDialog::new()
+                                .add_filter("JSON", &["json"])
+                                .pick_file()
+                        {
+                            if let Ok(data) = fs::read_to_string(&path) {
+                                if let Ok(template) = serde_json::from_str::<BackupTemplate>(&data) {
+                                    let mut valid = Vec::new();
+                                    let mut skipped = Vec::new();
 
-                            for p in template.paths {
-                                match fix_skip(&p) {
-                                    Some(adjusted) => valid.push(adjusted),
-                                    None => skipped.push(p),
+                                    for p in template.paths {
+                                        match fix_skip(&p) {
+                                            Some(adjusted) => valid.push(adjusted),
+                                            None => skipped.push(p),
+                                        }
+                                    }
+
+                                    self.selected_folders = valid;
+
+                                    let msg = if skipped.is_empty() {
+                                        "✅ Template loaded".into()
+                                    } else {
+                                        format!("✅ Loaded with {} paths skipped", skipped.len())
+                                    };
+
+                                    *self.status.lock().unwrap() = msg;
+                                } else {
+                                    *self.status.lock().unwrap() = "❌ Bad template format.".into();
                                 }
                             }
+                        }
+                    }
 
-                            self.selected_folders = valid;
-
-                            let msg = if skipped.is_empty() {
-                                "✅ Template loaded".into()
-                            } else {
-                                format!("✅ Loaded with {} paths skipped", skipped.len())
+                    if ui.button("Save Template").clicked() {
+                        if
+                            let Some(path) = FileDialog::new()
+                                .add_filter("JSON", &["json"])
+                                .save_file()
+                        {
+                            let template = BackupTemplate {
+                                paths: self.selected_folders.clone(),
                             };
 
-                            *self.status.lock().unwrap() = msg;
-                        } else {
-                            *self.status.lock().unwrap() = "❌ Bad template format.".into();
-                        }
-                    }
-                }
-            }
-
-            if ui.button("Save Template").clicked() {
-                if let Some(path) = FileDialog::new().add_filter("JSON", &["json"]).save_file() {
-                    let template = BackupTemplate {
-                        paths: self.selected_folders.clone(),
-                    };
-
-                    if let Ok(json) = serde_json::to_string_pretty(&template) {
-                        if fs::write(&path, json).is_ok() {
-                            *self.status.lock().unwrap() = "✅ Template saved.".into();
-                        } else {
-                            *self.status.lock().unwrap() = "❌ Failed to write template.".into();
-                        }
-                    }
-                }
-            }
-
-            if ui.button("Edit Template").clicked() {
-                if let Some(path) = FileDialog::new().add_filter("JSON", &["json"]).pick_file() {
-                    if let Ok(data) = fs::read_to_string(&path) {
-                        if let Ok(template) = serde_json::from_str::<BackupTemplate>(&data) {
-                            self.template_paths = template.paths
-                                .into_iter()
-                                .map(|p| fix_skip(&p).unwrap_or(p))
-                                .collect();
-                            self.template_editor = true;
-                        } else {
-                            *self.status.lock().unwrap() = "❌ Couldn't parse template.".into();
-                        }
-                    }
-                }
-            }
-
-            if ui.button("Create Backup").clicked() {
-                let folders = self.selected_folders.clone();
-                let status = self.status.clone();
-
-                if folders.is_empty() {
-                    *status.lock().unwrap() = "❌ Nothing selected.".into();
-                    return;
-                }
-
-                *status.lock().unwrap() = "Compressing into zip...".into();
-
-                thread::spawn(move || {
-                    if
-                        let Some(out_dir) = FileDialog::new()
-                            .set_title("Choose backup destination")
-                            .pick_folder()
-                    {
-                        match backup_gui(&folders, &out_dir) {
-                            Ok(path) => {
-                                *status.lock().unwrap() = format!(
-                                    "✅ Backup created:\n{}",
-                                    path.display()
-                                );
-                            }
-                            Err(e) => {
-                                *status.lock().unwrap() = format!("❌ Backup failed: {}", e);
+                            if let Ok(json) = serde_json::to_string_pretty(&template) {
+                                if fs::write(&path, json).is_ok() {
+                                    *self.status.lock().unwrap() = "✅ Template saved.".into();
+                                } else {
+                                    *self.status.lock().unwrap() =
+                                        "❌ Failed to write template.".into();
+                                }
                             }
                         }
-                    } else {
-                        *status.lock().unwrap() = "❌ Cancelled.".into();
+                    }
+
+                    if ui.button("Edit Template").clicked() {
+                        if
+                            let Some(path) = FileDialog::new()
+                                .add_filter("JSON", &["json"])
+                                .pick_file()
+                        {
+                            if let Ok(data) = fs::read_to_string(&path) {
+                                if let Ok(template) = serde_json::from_str::<BackupTemplate>(&data) {
+                                    self.template_paths = template.paths
+                                        .into_iter()
+                                        .map(|p| fix_skip(&p).unwrap_or(p))
+                                        .collect();
+                                    self.template_editor = true;
+                                } else {
+                                    *self.status.lock().unwrap() =
+                                        "❌ Couldn't parse template.".into();
+                                }
+                            }
+                        }
                     }
                 });
-            }
 
-            if ui.button("Restore Backup").clicked() {
-                let status = self.status.clone();
+                ui.vertical(|ui| {
+                    //backup
+                    if ui.button("Create Backup").clicked() {
+                        let folders = self.selected_folders.clone();
+                        let status = self.status.clone();
 
-                *status.lock().unwrap() = "Starting restore...".into();
-
-                thread::spawn(move || {
-                    if
-                        let Some(zip_file) = FileDialog::new()
-                            .add_filter("zip", &["zip"])
-                            .pick_file()
-                    {
-                        match restore_backup(&zip_file) {
-                            Ok(_) => {
-                                *status.lock().unwrap() = "✅ Restore complete.".into();
-                            }
-                            Err(e) => {
-                                *status.lock().unwrap() = format!("❌ Restore failed: {}", e);
-                            }
+                        if folders.is_empty() {
+                            *status.lock().unwrap() = "❌ Nothing selected.".into();
+                            return;
                         }
-                    } else {
-                        *status.lock().unwrap() = "❌ No file chosen.".into();
+
+                        *status.lock().unwrap() = "Compressing into zip...".into();
+
+                        thread::spawn(move || {
+                            if
+                                let Some(out_dir) = FileDialog::new()
+                                    .set_title("Choose backup destination")
+                                    .pick_folder()
+                            {
+                                match backup_gui(&folders, &out_dir) {
+                                    Ok(path) => {
+                                        *status.lock().unwrap() = format!(
+                                            "✅ Backup created:\n{}",
+                                            path.display()
+                                        );
+                                    }
+                                    Err(e) => {
+                                        *status.lock().unwrap() =
+                                            format!("❌ Backup failed: {}", e);
+                                    }
+                                }
+                            } else {
+                                *status.lock().unwrap() = "❌ Cancelled.".into();
+                            }
+                        });
+                    }
+
+                    if ui.button("Restore Backup").clicked() {
+                        let status = self.status.clone();
+
+                        *status.lock().unwrap() = "Starting restore...".into();
+
+                        thread::spawn(move || {
+                            if
+                                let Some(zip_file) = FileDialog::new()
+                                    .add_filter("zip", &["zip"])
+                                    .pick_file()
+                            {
+                                match restore_backup(&zip_file) {
+                                    Ok(_) => {
+                                        *status.lock().unwrap() = "✅ Restore complete.".into();
+                                    }
+                                    Err(e) => {
+                                        *status.lock().unwrap() =
+                                            format!("❌ Restore failed: {}", e);
+                                    }
+                                }
+                            } else {
+                                *status.lock().unwrap() = "❌ No file chosen.".into();
+                            }
+                        });
                     }
                 });
-            }
+            });
 
             ui.separator();
             ui.label(&*self.status.lock().unwrap());
