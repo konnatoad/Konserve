dd29ccb05b5b5665b2403a67eb6c71cc8c45a1c8
konnatoad <severi.moisare@gmail.com>
2025-04-19 01:12:30 +0300

    Force GUI mode in release Windows builds (if needed)

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 #[cfg(windows)]
 fn main() {
-    println!("cargo:rustc-link-arg=/SUBSYSTEM:windows");
+    // Only force GUI mode in release builds
+    if std::env::var("PROFILE").unwrap_or_default() == "release" {
+        println!("cargo:rustc-link-arg=/SUBSYSTEM:windows");
+    }
 
     let _ = embed_resource::compile("assets/windows.rc", embed_resource::NONE);
 }
