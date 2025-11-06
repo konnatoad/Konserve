46318309f5a5a2e5945e7389bc5aa3a7881fc8e5
konnatoad <severi.moisare@gmail.com>
2025-11-06 20:12:57 +0200

    Chore: update build.rs to satisfy clippy

--- build.rs
+++ build.rs
@@ -5,15 +5,15 @@ fn embed_fingerprint() {
 
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let env_path: PathBuf = [manifest_dir.as_str(), ".env"].iter().collect();
-    if let Ok(content) = fs::read_to_string(&env_path) {
-        if let Some(val) = content
-            .lines()
+
+    if let Some(val) = fs::read_to_string(&env_path).ok().and_then(|c| {
+        c.lines()
             .find_map(|line| line.trim_start().strip_prefix(&format!("{KEY}=")))
-        {
-            println!("cargo:rustc-env={KEY}={val}");
-            println!("cargo:rerun-if-changed={}", env_path.display());
-            println!("cargo:warning=build.rs saw FINGERPRINT=\"{val}\"");
-        }
+            .map(str::to_owned)
+    }) {
+        println!("cargo:rustc-env={KEY}={val}");
+        println!("cargo:rerun-if-changed={}", env_path.display());
+        println!("cargo:warning=build.rs saw FINGERPRINT=\"{val}\"");
     }
 }
 
